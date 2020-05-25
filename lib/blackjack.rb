059188ab0df8dcfb110a2c16be0dce9f56284cdc
Scott Espinosa <scottjames.espinosa@gmail.com>
2020-05-25 01:58:28 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -68,7 +68,7 @@ def runner
   card_total = initial_round
   while card_total < 21 do 
     card_total = hit?(card_total)
-    display_card_total (card_total)
+    display_card_total(card_total)
   end 
   if card_total > 21
     end_game(card_total)
